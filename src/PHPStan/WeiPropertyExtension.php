0734113323d23aa691631a649beb43b91774cb0d
twinh <twinhuang@qq.com>
2020-06-21 14:54:08 +0800

    feat: 增加 phpstan 和处理 wei 的扩展

---
+++ src/PHPStan/WeiPropertyExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Miaoxing\CodingStandards\PHPStan;
+
+use Miaoxing\CodingStandards\PHPStan\Reflection\WeiPropertyReflection;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\PropertiesClassReflectionExtension;
+use PHPStan\Reflection\PropertyReflection;
+use Wei\Wei;
+
+/**
+ * 允许通过 wei()->xxx 获取服务
+ *
+ * 对应错误：
+ *  "#^Access to an undefined property Wei\\\\Wei\\:\\:\\$(.+?)\\.$#"
+ */
+class WeiPropertyExtension implements PropertiesClassReflectionExtension
+{
+    public function __construct()
+    {
+        $alieses = wei()->classMap->generate(['src', 'plugins/*/src'], '/Service/*.php', 'Service');
+        wei()->setAliases($alieses);
+    }
+
+    public function hasProperty(ClassReflection $classReflection, string $propertyName): bool
+    {
+        return $classReflection->getName() === Wei::class
+            && !property_exists(Wei::class, $propertyName)
+            && \wei()->has($propertyName);
+    }
+
+    public function getProperty(ClassReflection $classReflection, string $propertyName): PropertyReflection
+    {
+        $class = wei()->getClass($propertyName);
+        return new WeiPropertyReflection($class, $classReflection);
+    }
+}
