5e8f49178c7c6a2960a6f7ed23145f98ec720951
twinh <twinhuang@qq.com>
2025-02-05 12:38:31 +0800

    test: 解决 PHP_CodeSniffer 定义了常量为字符创，PHP-Parser 检查为整数导致测试失败
    Token T_NAME_QUALIFIED has ID of type string, should be int. You may be using a library with broken token emulation
    
    https://github.com/squizlabs/PHP_CodeSniffer/blob/master/src/Util/Tokens.php
    https://github.com/nikic/PHP-Parser/blob/master/lib/PhpParser/compatibility_tokens.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,5 +12,10 @@ $loader = require $vendorDir . '/autoload.php';
 $loader->addPsr4('MiaoxingTest\Sniffs\\', __DIR__ . '/Sniffs');
 $loader->addPsr4('WordPressCS\\', $vendorDir . '/wp-coding-standards/wpcs');
 
+$compatFile = $vendorDir . '/nikic/php-parser/lib/PhpParser/compatibility_tokens.php';
+if (is_file($compatFile)) {
+    require_once $compatFile;
+}
+
 require_once $vendorDir . '/squizlabs/php_codesniffer/autoload.php';
 require_once $vendorDir . '/squizlabs/php_codesniffer/tests/bootstrap.php';
